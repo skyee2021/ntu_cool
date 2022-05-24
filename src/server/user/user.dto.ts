089c5245452efcd2e49b32d9e4c103e32f98e13c
skyee20211@gmail.com <skyee20211@gmail.com>
2022-05-24 17:44:12 +0800

    reg

--- src/server/user/user.dto.ts
+++ src/server/user/user.dto.ts
@@ -1,11 +1,14 @@
-import { IsNotEmpty, IsString, IsEmail } from 'class-validator';
+import { RequestMethod } from '@nestjs/common';
+import { IsNotEmpty, IsString, IsEmail, Matches } from 'class-validator';
+import { regPositive } from './regex.util';
 
 export class CreateUserDTO {
   readonly _id: string;
   @IsString()
   @IsNotEmpty()
   public readonly user_name: string;
-  @IsEmail(/^\S@\S$/)
+  // @IsEmail()
+  @Matches(regPositive, { message: 'wrong' })
   @IsString()
   @IsNotEmpty()
   public readonly user_email: string;
@@ -15,7 +18,8 @@ export class EditUserDTO {
   @IsString()
   @IsNotEmpty()
   public readonly user_name: string;
-  @IsEmail(/^\S@\S$/)
+  // @IsEmail()
+  @Matches(regPositive, { message: 'wrong' })
   @IsString()
   @IsNotEmpty()
   public readonly user_email: string;
